bc6ce64c0084adbd91434ac07f12288e32c4d89a
OliveiraAlbertCoelho <albertoliveira@pursuit.org>
2019-10-08 18:00:47 -0400

    done

--- Project/ProgrammaticUserCells/ProgrammaticUserCells/VC/UserDetailViewController.swift
+++ Project/ProgrammaticUserCells/ProgrammaticUserCells/VC/UserDetailViewController.swift
@@ -9,15 +9,43 @@
 import UIKit
 
 class UserDetailViewController: UIViewController {
-    var user: User?
+    var user: User!
     override func viewDidLoad() {
-        super.viewDidLoad()
+        setUpLabel()
         self.view.addSubview(userName)
-        view.backgroundColor = .red
+        self.view.addSubview(address)
+        self.view.addSubview(email)
+        self.view.backgroundColor = .white
+        addConstraints()
+        super.viewDidLoad()
     }
-    var userName: UILabel = {
-        let name = UILabel(frame:CGRect(x: 100, y: 100, width: 20, height: 30))
+    func setUpLabel(){
+        userName.text = "\(user.name.title) \(user.name.first) \(user.name.last) "
+        email.text = user.email
+        address.text = "\(user.location.street) \(user.location.city)"
+    }
+    lazy var userName: UILabel = {
+        let name = UILabel(frame:CGRect(x: 0, y: 350, width: 400, height: 400))
            return name
        }()
+    lazy var email: UILabel = {
+        let email = UILabel(frame: CGRect(x: 0, y: 150, width: 400, height: 400))
+        return email
+    }()
+    lazy var address: UILabel = {
+        let address = UILabel(frame: CGRect(x: 0, y: 350, width: 400, height: 400))
+        return address
+    }()
     
+    func addConstraints(){
+    self.userName.translatesAutoresizingMaskIntoConstraints = false
+       NSLayoutConstraint.activate(
+       [userName.centerYAnchor.constraint(equalTo: self.view.centerYAnchor , constant: 0),
+       userName.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0),
+       email.centerYAnchor.constraint(equalTo: self.view.centerYAnchor , constant: 0),
+       email.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0),
+       address.centerYAnchor.constraint(equalTo: self.view.centerYAnchor , constant: 0),
+       address.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0),
+        ])
+    }
 }
